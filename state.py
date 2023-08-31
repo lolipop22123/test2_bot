4f3809e09a2e6c1250be23e5262ec6fa42343c96
lolipop22123 <77731660+lolipop22123@users.noreply.github.com>
2023-09-01 02:03:21 +0300

    Add files via upload

---
+++ state.py
@@ -0,0 +1,7 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class AddProductState(StatesGroup):
+    name = State()        # Состояние для ввода названия товара
+    description = State() # Состояние для ввода описания товара
+     ###
